c2cfebab039a7b3202480f3e5c63a9a666bc7e6f
luspadzijanski <97367273+luspadzijanski@users.noreply.github.com>
2022-03-12 19:08:01 +0100

    Add files via upload

---
+++ square.py
@@ -0,0 +1,14 @@
+def Square(numrows):
+    i = 0
+    j = 0
+    for i in range(numrows):
+        for j in range(numrows):
+            if(i == 0 or i == numrows-1 or j == 0 or j == numrows-1):{
+                print('*', end = '  ')
+            }
+            else:{
+                print(' ', end = '  ')
+            }
+        print()
+        
+Square(5)
